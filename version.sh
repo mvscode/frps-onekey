7ce60ab848138067fabfcb794c0d2bcc33472333
Jacko1045 <37238560+Jacko1045@users.noreply.github.com>
2019-01-18 12:52:53 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export FRPS_VER=https://api.github.com/repos/fatedier/frp/releases/latest
+export FRPS_VER=0.23.1
 export FRPS_INIT="https://raw.githubusercontent.com/jacko1045/frp-onekey/master/frps.init"
 export aliyun_download_url="https://code.aliyun.com/jacko1045/frp/raw/master"
 export github_download_url="https://api.github.com/repos/fatedier/frp/releases/latest"
