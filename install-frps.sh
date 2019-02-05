1415130692e10dc7ddc582227c9e8558a1ef9bc8
Jacko1045 <37238560+Jacko1045@users.noreply.github.com>
2019-02-05 22:10:58 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
 export FRPS_VER=0.23.3
-export FRPS_INIT="https://raw.githubusercontent.com/jacko1045/frp-onekey/test/frps.init"
+export FRPS_INIT="https://raw.githubusercontent.com/jacko1045/frp-onekey/master/frps.init"
 export aliyun_download_url="https://code.aliyun.com/jacko1045/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
 #======================================================================
