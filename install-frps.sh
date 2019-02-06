37bda74494c87cc8d16f039372359759e8396a9e
MvsCode <37238560+MvsCode@users.noreply.github.com>
2019-02-06 12:09:15 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -3,8 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
 export FRPS_VER=0.23.3
-export FRPS_INIT="https://raw.githubusercontent.com/jacko1045/frp-onekey/master/frps.init"
-export aliyun_download_url="https://code.aliyun.com/jacko1045/frp/raw/master"
+    export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frp-onekey/master/frps.init"
+export aliyun_download_url="https://code.aliyun.com/MvsCode/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
 #======================================================================
 #   System Required:  CentOS Debian or Ubuntu (32bit/64bit)
@@ -18,7 +18,7 @@ str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
 ver_file="/tmp/.frp_ver.sh"
-str_install_shell="https://raw.githubusercontent.com/jacko1045/frp-onekey/master/install-frps.sh"
+str_install_shell="https://raw.githubusercontent.com/MvsCode/frp-onekey/master/install-frps.sh"
 shell_update(){
     fun_clangcn "clear"
     echo "Check updates for shell..."
