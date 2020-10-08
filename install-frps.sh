e2a8be81b4606e48bb4e15a29016d8157e6c5a19
MvsCode <37238560+MvsCode@users.noreply.github.com>
2020-10-08 20:56:05 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
-export FRPS_VER=0.34.0
+export FRPS_VER=0.34.1
 export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/frps.init"
 export aliyun_download_url="https://code.aliyun.com/mvscode/frps-onekey/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
