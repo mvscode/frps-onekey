8b52a617637fe74e1254dd6e6a705a35adf9a46d
MvsCode <37238560+MvsCode@users.noreply.github.com>
2021-01-25 23:46:49 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -5,7 +5,7 @@ export PATH
 export FRPS_VER=0.35.1
 export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/frps.init"
 export aliyun_download_url="https://code.aliyun.com/MvsCode/frps-onekey/raw/master"
-export gitee_download_url="https://gitee.com/mvscode/frps-onekey/raw/master"
+export gitee_download_url="https://gitee.com/mvscode/frps-onekey/attach_files/598671/download/frp_0.35.1_linux_amd64.tar.gz"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
 #======================================================================
 #   System Required:  CentOS Debian Ubuntu or Fedora(32bit/64bit)
