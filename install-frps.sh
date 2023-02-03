be9b4f0b94b5e62dabf155c0734f0dc29dcf8ce0
MvsCode <37238560+MvsCode@users.noreply.github.com>
2023-02-03 21:30:52 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -13,7 +13,7 @@ export github_download_url="https://github.com/fatedier/frp/releases/download"
 #   Mender : MvsCode
 #======================================================================
 program_name="frps"
-version="230115"
+version="230203"
 str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
