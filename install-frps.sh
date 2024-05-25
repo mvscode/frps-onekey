a2fd2dd18e97ca670924e7be243329230ba962a1
MvsCode <37238560+MvsCode@users.noreply.github.com>
2024-05-25 18:52:02 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -843,9 +843,6 @@ update_program_server_clang(){
         install_shell=${strPath}
 # Check if the remote_init_version variable is not empty
 if [ -n "${remote_init_version}" ]; then
-    # Code block to execute if the condition is true
-    ...
-fi
             if [[ "${local_init_version}" != "${remote_init_version}" ]];then
                 echo "========== Update ${program_name} ${program_init} =========="
                 if ! wget  ${FRPS_INIT} -O ${program_init}; then
