abf48c92bc5bdbe8405157a8d2178fa99c1e59dc
MvsCode <37238560+MvsCode@users.noreply.github.com>
2024-06-11 00:24:08 +0800

    更新 install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -578,7 +578,7 @@ cat <<- EOF >> "${str_program_dir}/${program_config_file}"
 # A literal address or host name for IPv6 must be enclosed
 # in square brackets, as in "[::1]:80", "[ipv6-host]:http" or "[ipv6-host%zone]:80"
 # For single "bindAddr" field, no need square brackets, like `bindAddr = "::"`.
-bindAddr = "0.0.0.0"
+bindAddr = 0.0.0.0
 bindPort = ${set_bindPort}
 
 # udp port used for kcp protocol, it can be same with 'bindPort'.
@@ -715,7 +715,7 @@ cat <<- EOF >> "${str_program_dir}/${program_config_file}"
 # A literal address or host name for IPv6 must be enclosed
 # in square brackets, as in "[::1]:80", "[ipv6-host]:http" or "[ipv6-host%zone]:80"
 # For single "bindAddr" field, no need square brackets, like `bindAddr = "::"`.
-bindAddr = "0.0.0.0"
+bindAddr = 0.0.0.0
 bindPort = ${set_bindPort}
 
 # udp port used for kcp protocol, it can be same with 'bindPort'.
