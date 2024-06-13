7a85a22271255dce4496be16bd4a40fcd0b3e3cd
MvsCode <37238560+MvsCode@users.noreply.github.com>
2024-06-13 15:28:03 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -641,6 +641,7 @@ vhostHTTPSPort = ${set_vhost_https_port}
 
 # Configure the web server to enable the dashboard for frps.
 # dashboard is available only if webServerport is set.
+webServer.addr = "0.0.0.0"
 webServer.port = ${set_dashboard_port}
 webServer.user = "${set_dashboard_user}"
 webServer.password = "${set_dashboard_pwd}"
